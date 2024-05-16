77bde150fd907a4014343ef4192ea3d07104e68f
Sergi-Virgili <svirgilif@gmail.com>
2024-05-16 14:16:26 +0200

    add ionput name

--- 01_Hello/hello.py
+++ 01_Hello/hello.py
@@ -1,2 +1,6 @@
-# invoca la funcion print() para escribir en la terminal
-print("Hello World")
+
+# asignar a name el input de consola. (Pausar Flujo)
+name = input("Dime tu nombre: ")
+
+# invoca la funcion print() para escribir en la terminal World conscantenado con el valor de la var name
+print("Hello", name)
