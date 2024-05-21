7bef45a419494c50bf54e3a13034173e1e877fa8
Sergi-Virgili <svirgilif@gmail.com>
2024-05-21 15:02:38 +0200

    userlist crud

--- 01_Hello/hello.py
+++ 01_Hello/hello.py
@@ -1,6 +1,19 @@
+users = [
+    {"name": "Alice", "age": 25, "phone": "628546595"},
+    {"name": "Bob", "age": 30, "phone": "628546596"},
+    {"name": "Charlie", "age": 35, "phone": "628546597"},
+]
 
-# asignar a name el input de consola. (Pausar Flujo)
-name = input("Dime tu nombre: ")
+# pide el nombre, la edad y el telefono lo guarda en la lista de usuarios
+name = input("Name: ")
+age = int(input("Age: "))
+phone = input("Phone: ")
 
-# invoca la funcion print() para escribir en la terminal World conscantenado con el valor de la var name
-print("Hello", name)
+users.append({"name": name, "age": age, "phone": phone})
+
+# Print the header
+print(" | ".join(users[0].keys()))
+
+# Print the users
+for user in users:
+    print(f"{user['name']} | {user['age']} | {user['phone']}")
